31a63b2ce2f37f4d74d8b0dd49c27d90cb5839d0
Abdallah Monzer <presskeyword@gmail.com>
2017-10-15 22:14:55 +0300

    Add files via upload

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -5,6 +5,6 @@ namespace App\Http\Controllers;
 class TasksController extends Controller
 {
     public function index(){
-        return view('tasks');
+        return view('tasks', array('page'=>'project'));
     }
 }
